e21dfde7bcb6c3faa6066f3a20d870e3a7642eba
William Zhang <willzhang05@gmail.com>
2022-06-19 19:30:12 -0400

    remove other stuff from bridge

--- armv7_compile.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-CARGO_TARGET_ARMV7_UNKNOWN_LINUX_GNUEABIHF_LINKER=/usr/bin/arm-linux-gnueabihf-gcc cargo build --release --target=armv7-unknown-linux-gnueabihf
--- wrapper.h
+++
@@ -1,3 +0,0 @@
-#include "CAN-messages/rivanna2.h"
-#include "CAN-messages/bps.h"
-#include "CAN-messages/motor_controller.h"
