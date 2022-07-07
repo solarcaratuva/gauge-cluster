4aee6ead9ebb45b63f7cea491027e42fb088f210
William Zhang <willzhang05@gmail.com>
2022-07-07 04:41:00 +0000

    fix logging to flash drive, add ohter configs

--- log.sh
+++ log.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-cd /run/media/alarm/LOGGING
-
-candump -l can0
+LOGGING_DIR=/run/media/alarm/LOGGING
+PREFIX=rivanna2
+IFACE=can0
+(cd $LOGGING_DIR && candump -L $IFACE > $(date +"$PREFIX-%Y_%m_%d_%I_%M_%p").log)
